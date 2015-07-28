73621a63922769e6271883e893480f36bf6b530e
Charlie McElfresh <charlie.mcelfresh@socialchorus.com>
2015-07-27 21:07:25 -0700

    Charlie's comments

--- programming_tests/0001/transform2.rb
+++ programming_tests/0001/transform2.rb
@@ -8,11 +8,25 @@ Transform.new.count
 will be output. (See spec/test_0001_spec.rb to see the exact output)
 
 =end 
+
+## This is good straightforward work. Well done! None of the following is meant as criticism:
+## you might want to be aware of a couple shortcuts:
+## 
 class Transform2
   def count
     count = Hash.new
     count.default = 0 #this is key, without this you cannot increment as you step through the file
+
+    # instead of the two above lines, you can just say count = Hash.new(0), that sets 0 as the 
+    # default value for every new key
+
     f = File.open("./programming_tests/input_files/transform2.txt", "r")
+
+    ## The below block is just fine as it is. Another option is to use something like map, or
+    ## inject, or some other handy Ruby function, or a few functions chained together, to get
+    ## the same result. You might want to try to do this, and if you do, send it to me and
+    ## I'll take a look.
+
     f.each do |line|
       count[line.strip]+=1 #strip the \n from each line
     end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,2 +1,9 @@
- @files = Dir.glob(File.join(File.dirname(__FILE__), "../programming_tests/0001/*")).map{|f| require(f)}
+# you don't need this @files instance variable here -- and you never want to have a space before
+# line like you have. You can just say
+
+Dir.glob(File.join(File.dirname(__FILE__), "../programming_tests/0001/*")).map{|f| require(f)}
+
+# also -- where is my byebug? Take another look at the fix_tests.rb file - I want to be able to
+# put byebug statements in my tests so I can debug them. I put a byebug statement in my tests.
+# Run the tests again and see what happens.
  
